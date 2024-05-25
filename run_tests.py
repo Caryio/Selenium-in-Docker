30da5f403178f169b9c3f51f172bcfef4579798e
CaryioCAT <41476501+Caryio@users.noreply.github.com>
2024-05-25 17:35:04 +0800

    Create run_tests.py

---
+++ run_tests.py
@@ -0,0 +1,17 @@
+import pytest
+
+def run_login_tests():
+    pytest.main(["-v", "--html=report.html", "-m", "login", "tests/"])
+
+def run_register_tests():
+    pytest.main(["-v", "--html=report.html", "-m", "register", "tests/"])
+
+def run_change_password_tests():
+    pytest.main(["-v", "--html=report.html", "-m", "change_password", "tests/"])
+
+if __name__ == "__main__":
+    run_login_tests()
+
+    run_register_tests()
+
+    run_change_password_tests()
