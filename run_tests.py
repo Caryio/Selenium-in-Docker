f72b7b707dc92325153c8bb1dfefce73f22826f2
CaryioCAT <41476501+Caryio@users.noreply.github.com>
2024-05-25 17:39:06 +0800

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -9,9 +9,14 @@ def run_register_tests():
 def run_change_password_tests():
     pytest.main(["-v", "--html=report.html", "-m", "change_password", "tests/"])
 
+def run_all_tests():
+    pytest.main(["-v", "--html=report.html", "tests/"])
+
 if __name__ == "__main__":
     run_login_tests()
 
-    run_register_tests()
+    # run_register_tests()
 
-    run_change_password_tests()
+    # run_change_password_tests()
+
+    # run_all_tests()
